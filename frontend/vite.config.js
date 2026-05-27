78f42fbcd039e2904fe5aed42401873025753384
LeeTech-LK <lukatokalaluka@gmail.com>
2026-05-27 13:33:33 +0200

    Update vite.config.js with process

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  const apiProxyTarget = env.VITE_API_BASE || 'http://localhost:3001'
+  const apiProxyTarget = process.env.VITE_API_BASE || 'http://localhost:3001'
 
   return defineConfig({
     plugins: [react()],
